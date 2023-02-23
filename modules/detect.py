486945b98d6473e3c7606637a4901bc44f796173
sidu21 <125720418+sidu21@users.noreply.github.com>
2023-02-23 10:46:43 +0000

    yo

--- modules/detect.py
+++ modules/detect.py
@@ -50,11 +50,11 @@ class ImageDetect:
                 # Here, labels start from 1, subtract 1 
                 labels = outputs['classes']
                 scores = outputs['scores']
-
+                print(self.class_names(labels))
                 boxes_result.append(boxes)
                 labels_result.append(labels)
                 scores_result.append(scores)
-                print(outputs)
+                
         return {
             "boxes": boxes_result, 
             "labels": labels_result,

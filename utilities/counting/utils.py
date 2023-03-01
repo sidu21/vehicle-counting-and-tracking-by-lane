5fb2d59ce87c9b8794e2da0ad063d10e202becda
sidu21 <125720418+sidu21@users.noreply.github.com>
2023-03-01 11:02:09 +0000

    ch

--- utilities/counting/utils.py
+++ utilities/counting/utils.py
@@ -309,7 +309,8 @@ def visualize_merged(videoloader, csv_path, directions, zones, num_classes, outv
         } for dir in directions
     }
 
-    
+    print(count_dict)
+
 
     prev_text = None # Delay direction text by one frame
     for batch in tqdm(videoloader):
